5b416c4c69b6b7af0b324884f5a02c559c3f6421
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-12-20 22:17:19 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,36 @@
+# test.py
+
+
+from nanoasgi import App
+from async_asgi_testclient import TestClient
+import pytest
+
+
+app = App()
+
+
+@app.on('startup')
+async def on_startup():
+    print('Ready to serve requests')
+
+
+@app.on('shutdown')
+async def on_shutdown():
+    print('Shutting down')
+
+
+@app.route('GET', '/api/hello/{name}/')
+async def hello_handler(request, name):
+    return Response(
+        {'result': f'Hello {name}!'},
+        status=200,
+        headers=[('Content-Type', 'application/json')],
+    )
+
+
+@pytest.mark.asyncio
+async def test_quart_app():
+    async with TestClient(app) as client:
+        resp = await client.get('/api/hello/World/')
+        assert resp.status_code == 200
+        assert resp.json() == {'result': 'Hello World!'},
